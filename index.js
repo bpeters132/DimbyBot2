ee8eca1a946df32b8dd29f2472cd21ea63c769d1
Brandt <b.peters132@gmail.com>
2021-02-10 16:21:48 -0600

    refactoring

--- index.js
+++ index.js
@@ -1,64 +1,59 @@
-require('dotenv').config()
-const {CommandoClient} = require('discord.js-commando')
-const path = require('path')
-const unirest = require('unirest')
-const io = require('@pm2/io')
-
-
-const token = process.env.token
-const prefix = process.env.prefix
-const owner_id = process.env.owner_id
+const { CommandoClient } = require("discord.js-commando");
+const { prefix, token, owner_id } = require("./config.json");
+const path = require("path");
+const unirest = require("unirest");
+const io = require("@pm2/io");
 
 const client = new CommandoClient({
   commandPrefix: prefix,
   owner: owner_id,
-  unknownCommandResponse: false
-})
+  unknownCommandResponse: false,
+});
 
 client.registry
   .registerDefaultTypes()
   .registerGroups([
-    ['fun', 'Commands For Fun'],
-    ['moderation', 'Moderation Commands']
+    ["fun", "Commands For Fun"],
+    ["moderation", "Moderation Commands"],
+    ["musicc", "Music Commands"],
   ])
   .registerDefaultGroups()
   .registerDefaultCommands()
-  .registerCommandsIn(path.join(__dirname, 'commands'))
+  .registerCommandsIn(path.join(__dirname, "commands"));
 
+client.once("ready", () => {
+  console.log(`Logged in as ${client.user.tag}! (${client.user.id})`);
+  client.user.setActivity(
+    `${prefix}help | Running on ${client.guilds.cache.size} servers`
+  );
+});
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}! (${client.user.id})`)
-    client.user.setActivity(
-      `${prefix}help | Running on ${client.guilds.cache.size} servers`
-    )
-})
+client.on("message", (message) => {
+  if (message.author.bot) return;
 
-client.on('message', message => {
-  if (message.author.bot) return
+  const request = unirest("GET", `https://pingdat.io/?t=dmbybtmsgsnd4574&v=1`);
+  request.end(function (response) {
+    if (response.error) {
+      message.reply("An error has occurred, please contact the bot owner.");
+      return console.error("GET error: ", response.error);
+    }
+    console.log("Pinged Pingdat!");
+  });
 
-  const request = unirest("GET", `https://pingdat.io/?t=dmbybtmsgsnd4574&v=1`)
-        request.end(function (response) {
-            if (response.error){
-                message.reply('An error has occurred, please contact the bot owner.')
-                return console.error('GET error: ', response.error)
-            }
-            console.log("Pinged Pingdat!")
-        })
-
-  if ((message.content).toLowerCase() === 'no u'){
-    message.channel.send('no u')
+  if (message.content.toLowerCase() === "no u") {
+    message.channel.send("no u");
   }
 
-  if ((message.content).toLowerCase() === 'u no'){
-    message.channel.send({files: ["https://i.imgflip.com/2rytcz.jpg"]})
+  if (message.content.toLowerCase() === "u no") {
+    message.channel.send({ files: ["https://i.imgflip.com/2rytcz.jpg"] });
   }
-})
+});
 
 io.init({
   transactions: true, // will enable the transaction tracing
-  http: true // will enable metrics about the http server (optional)
-})
+  http: true, // will enable metrics about the http server (optional)
+});
 
-client.on('error', console.error)
+client.on("error", console.error);
 
-client.login(token)
+client.login(token);
