b18524f289e2dfd6597e5127d63f73287c7278e5
Brandt <b.peters132@gmail.com>
2021-02-22 22:49:30 -0600

    Update stocksearch.js

--- commands/stocks/stocksearch.js
+++ commands/stocks/stocksearch.js
@@ -17,7 +17,7 @@ module.exports = class UrbanCommand extends Command {
                     prompt: "Specify the stock's symbol",
                     type: "string",
                     validate: (symbol) =>
-                        symbol.length >= 3 && symbol.length <= 4,
+                        symbol.length >= 2 && symbol.length <= 4,
                 },
             ],
         });
