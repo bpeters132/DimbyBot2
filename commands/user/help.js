d6719c8a0a6365b39a912687dcfc2da36822ca9d
Brandt <b.peters132@gmail.com>
2021-09-13 23:25:33 -0500

    humena

--- commands/user/help.js
+++ commands/user/help.js
@@ -1,43 +1,47 @@
-const { prefix } = require("../../config.json");
+const prefix = process.env.PREFIX;
 
 module.exports = {
-  name: "help",
-  description: "List all of my commands or info about a specific command.",
-  cooldown: 5,
-  aliases: ["commands"],
-  usage: "[command name]",
-  async execute(message, args) {
-    const data = [];
-    const { commands } = message.client;
-    if (!args.length) {
-      data.push("Here's a list of all my commands:");
-      data.push(`\`\`\`${commands.map((command) => command.name).join(", ")}\`\`\``);
-      data.push(
-        `You can send \`${prefix}help [command name]\` to get info on a specific command!`
-      );
-      response = data.join("\n")
-      return message.reply(response);
-    }
-    const name = args[0].toLowerCase();
-    const command =
-      commands.get(name) ||
-      commands.find((c) => c.aliases && c.aliases.includes(name));
+    name: "help",
+    description: "List all of my commands or info about a specific command.",
+    cooldown: 5,
+    aliases: ["commands"],
+    usage: "[command name]",
+    async execute(message, args) {
+        const data = [];
+        const { commands } = message.client;
+        if (!args.length) {
+            data.push("Here's a list of all my commands:");
+            data.push(
+                `\`\`\`${commands
+                    .map((command) => command.name)
+                    .join(", ")}\`\`\``
+            );
+            data.push(
+                `You can send \`${prefix}help [command name]\` to get info on a specific command!`
+            );
+            response = data.join("\n");
+            return message.reply(response);
+        }
+        const name = args[0].toLowerCase();
+        const command =
+            commands.get(name) ||
+            commands.find((c) => c.aliases && c.aliases.includes(name));
 
-    if (!command) {
-      return message.reply("That's not a valid command!");
-    }
+        if (!command) {
+            return message.reply("That's not a valid command!");
+        }
 
-    data.push(`**Name:** ${command.name}`);
+        data.push(`**Name:** ${command.name}`);
 
-    if (command.aliases)
-      data.push(`**Aliases:** ${command.aliases.join(", ")}`);
-    if (command.description)
-      data.push(`**Description:** ${command.description}`);
-    if (command.usage)
-      data.push(`**Usage:** ${prefix}${command.name} ${command.usage}`);
+        if (command.aliases)
+            data.push(`**Aliases:** ${command.aliases.join(", ")}`);
+        if (command.description)
+            data.push(`**Description:** ${command.description}`);
+        if (command.usage)
+            data.push(`**Usage:** ${prefix}${command.name} ${command.usage}`);
 
-    data.push(`**Cooldown:** ${command.cooldown || 3} second(s)`);
-    response = data.join("\n")
-    message.reply(response);
-  },
+        data.push(`**Cooldown:** ${command.cooldown || 3} second(s)`);
+        response = data.join("\n");
+        message.reply(response);
+    },
 };
--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const Discord = require("discord.js");
 require("dotenv").config();
 
-const prefix = process.env.PREFIX
+const prefix = process.env.PREFIX;
 
 const client = new Discord.Client({
     intents: [
