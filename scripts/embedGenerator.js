6040b101a37d97718c27cf9ca37ed78fd6bfbfd2
bpeters132 <b.peters132@gmail.com>
2021-09-22 14:47:10 -0500

    Added function to dynamically create Rich Embeds

---
+++ scripts/embedGenerator.js
@@ -0,0 +1,22 @@
+const { MessageEmbed } = require('discord.js');
+
+module.exports.general = (title, description, arrFieldNames, arrFieldValues) => {
+    return new Promise((resolve) => {
+        if (typeof (arrFieldNames) != 'undefined') {
+            if (arrFieldNames.length != arrFieldValues.length && arrFieldNames < 1) {
+                const response = new MessageEmbed()
+                    .setTitle(title)
+                    .setDescription(description);
+                for (let i = 0; i < arrFieldNames.length; i++) {
+                    response.addField(arrFieldNames[i], arrFieldValues[i]);
+                }
+                resolve(response);
+            }
+        } else {
+            const response = new MessageEmbed()
+                .setTitle(title)
+                .setDescription(description);
+            resolve(response);
+        }
+    });
+};
