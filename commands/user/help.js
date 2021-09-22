856df7b7252e05ecdc2b4aabd09076e119983ca7
bpeters132 <b.peters132@gmail.com>
2021-09-22 14:57:50 -0500

    Update help.js
    Making use of embed generator

--- commands/user/help.js
+++ commands/user/help.js
@@ -1,5 +1,5 @@
 const prefix = process.env.PREFIX;
-const { MessageEmbed } = require('discord.js');
+const egenerator = require('../../scripts/embedGenerator');
 module.exports = {
     name: 'help',
     description: 'List all of my commands or info about a specific command.',
@@ -7,27 +7,33 @@ module.exports = {
     aliases: ['commands'],
     usage: '[command name]',
     async execute(client, message, args) {
-
         const { commands } = client;
         if (!args.length) {
-            const response = new MessageEmbed()
-                .setTitle('Here\'s a list of all my commands:')
-                .setDescription(
-                    `You can send \`${prefix}help [command name]\` to get info on a specific command!\n`,
-
-                )
-                .addFields(
-                    {
-                        name: 'Commands', value: `${commands
-                            .map((command) => command.name)
-                            .join(', ')}`
-                    },
-                    {
-                        name: 'Music', value: 'I can also play music! Type `/` and look for me in the command window!'
-                    }
-                );
+            console.log('1');
+            // Build Variables for response
+            let resTitle = 'Here\'s a list of all my commands:';
+            console.log('1');
+            let resDesc = `You can send \`${prefix}help [command name]\` to get info on a specific command!`;
+            console.log('1');
+            let arrFieldNames = [
+                'Commands',
+                'Music'
+            ];
+            console.log('1');
+            let arrFieldValues = [
+                `${commands.map((command) => command.name).join(', ')}`,
+                'I can also play music! Type `/` and look for me in the command window!'
+            ];
+            console.log('1');
+            
+            // Generate Embeded response with built variables
+            const response = await egenerator.general(resTitle, resDesc, arrFieldNames, arrFieldValues);
+            console.log('1');
+            // Send Response
             return message.channel.send({ embeds: [response] });
         }
+
+        // Continue and build help if specific command is specified
         const name = args[0].toLowerCase();
         const command =
             commands.get(name) ||
@@ -37,18 +43,28 @@ module.exports = {
             return message.reply('That\'s not a valid command!');
         }
 
-        const response = new MessageEmbed().setTitle(command.name.charAt(0).toUpperCase() + command.name.slice(1));
+        // Build Variables for response
+        let resTitle = command.name.charAt(0).toUpperCase() + command.name.slice(1);
+        let resDesc = command.description;
+        let arrFieldNames = [];
+        let arrFieldValues = [];
+
         if (command.aliases) {
-            response.addField('Aliases', command.aliases.join(', '));
-        }
-        if (command.description) {
-            response.addField('Description', command.description);
+            arrFieldNames.push('Aliases');
+            arrFieldValues.push(command.aliases.join(', '));
         }
         if (command.usage) {
-            response.addField('Usage', `${prefix}${command.name} ${command.usage}`);
+            arrFieldNames.push('Usage');
+            arrFieldValues.push(`${prefix}${command.name} ${command.usage}`);
         }
-        response.addField('Cooldown', `${command.cooldown || 3} second(s)`);
 
+        arrFieldNames.push('Cooldown');
+        arrFieldValues.push(`${command.cooldown || 3} second(s)`);
+
+        // Generate Embeded response with built variables
+        const response = await egenerator.general(resTitle, resDesc, arrFieldNames, arrFieldValues);
+
+        // Send Response
         message.channel.send({ embeds: [response] });
     },
 };
