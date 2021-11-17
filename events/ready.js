d926c94eeb474c688865f9755c15c0975f065a49
bpeters132 <b.peters132@gmail.com>
2021-11-17 15:18:30 -0600

    removing useless player commands and fixing the status from disappearing

--- events/ready.js
+++ events/ready.js
@@ -1,5 +1,8 @@
 module.exports = async (client) => {
     console.log(`Logged in as ${client.user.tag}! (${client.user.id})`);
     client.user.setActivity(`${process.env.PREFIX}help | Serving the People`);
-    
+    setInterval((client) => {
+        client.user.setActivity(`${process.env.PREFIX}help | Serving the People`);
+    }, 21600*1000);
+       
 };
