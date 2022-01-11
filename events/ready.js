4e42729a42e38946cfe942d7c2c258d443a4bba5
Brandt <b.peters132@gmail.com>
2022-01-10 18:13:58 -0600

    added login tracking

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,9 @@
+const pingDat = require('../lib/pingDat.js');
+
 module.exports = async (client) => {
     console.log(`Logged in as ${client.user.tag}! (${client.user.id})`);
     client.user.setActivity(`${process.env.PREFIX}help | Serving the People`);
+    pingDat.ping('https://pingdat.io/dp1?t=9bx27nMPtqNqjxNs&v=1');
     setInterval((client) => {
         client.user.setActivity(`${process.env.PREFIX}help | Serving the People`);
     }, 21600*1000);
