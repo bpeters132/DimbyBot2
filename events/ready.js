5bcd36ef14c0bf2ff3b8a989719bd071f4fd3d21
bpeters132 <b.peters132@gmail.com>
2022-01-26 16:59:48 -0600

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -2,10 +2,13 @@ const pingDat = require('../lib/pingDat.js');
 
 module.exports = async (client) => {
     console.log(`Logged in as ${client.user.tag}! (${client.user.id})`);
-    client.user.setActivity(`${process.env.PREFIX}help | Serving the People`);
     pingDat.ping(process.env.PING_LOGIN_URL);
+    client.user.setActivity(`${process.env.PREFIX}help | Serving the People`);
     setInterval((client) => {
         client.user.setActivity(`${process.env.PREFIX}help | Serving the People`);
     }, 21600*1000);
+    setInterval(() => {
+        console.log('Doing a thing');
+    });
        
 };
