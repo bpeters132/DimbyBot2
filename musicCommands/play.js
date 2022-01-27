3780eece4e0205365b3e274cc47206b5f3b8d37b
Brandt <b.peters132@gmail.com>
2022-01-26 20:17:06 -0600

    Update play.js

--- musicCommands/play.js
+++ musicCommands/play.js
@@ -26,7 +26,7 @@ module.exports = class extends SlashCommand {
 
         const { client } = require('../app.js');
         await ctx.defer();
-        
+
         const guild = client.guilds.cache.get(ctx.guildID);
         const channel = guild.channels.cache.get(ctx.channelID);
         const query = ctx.options.query;
@@ -41,42 +41,45 @@ module.exports = class extends SlashCommand {
                 console.log('Query Failed!');
                 console.error(err);
             });
-        if (!searchResult || !searchResult.tracks.length){
+        if (!searchResult || !searchResult.tracks.length) {
             console.log('No Search Results Found!');
             return void ctx.sendFollowUp({ content: 'No results were found!' });
         }
         console.log('Results found.');
-        console.log('Creating Queue...');
+        console.log('Getting Queue...');
         const queue = await client.player.createQueue(guild, {
             metadata: channel,
         });
-        console.log('Queue created!');
+        console.log('Queue attained/created!');
         const member = guild.members.cache.get(ctx.user.id) ?? await guild.members.fetch(ctx.user.id);
         try {
-            console.log('Connecting to member voice channel...');
-            if (!queue.connection) await queue.connect(member.voice.channel);
-            console.log('Connected to voice channel');
+            if (!queue.connection) {
+                console.log('Connecting to member voice channel...');
+                await queue.connect(member.voice.channel);
+                console.log('Connected to voice channel');
+            }
         } catch {
             console.log('Unable to join voice channel');
+            console.log('Destryoing queue');
             void client.player.deleteQueue(ctx.guildID);
+            console.log('Queue destroyed');
             return void ctx.sendFollowUp({ content: 'Could not join your voice channel!' });
         }
         await ctx.sendFollowUp({ content: `⏱ | Loading your ${searchResult.playlist ? 'playlist' : 'track'}...` });
-        if (searchResult.playlist){
+        if (searchResult.playlist) {
             console.log('Adding playlist to queue...');
             queue.addTracks(searchResult.tracks);
-            channel.send({content: `<@${ctx.user.id}>, Playlist queued!`});
+            channel.send({ content: `<@${ctx.user.id}>, Playlist queued!` });
             console.log('Playlist queued!');
-        }else{
+        } else {
             console.log('Adding track to queue...');
             queue.addTrack(searchResult.tracks[0]);
             console.log('Track added to queue!');
         }
         // searchResult.playlist ? queue.addTracks(searchResult.tracks) : queue.addTrack(searchResult.tracks[0]);
         if (!queue.playing) {
-            console.log('Starting to play...');
+            console.log('Telling queue to play...');
             await queue.play();
-            console.log('Playing!');
         }
     }
 };
