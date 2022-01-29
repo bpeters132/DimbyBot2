3633dec589668c79807639d97380f0f1811e6ef4
Brandt <b.peters132@gmail.com>
2022-01-29 11:23:59 -0600

    removing pm2 io cause it's useless in this usecase

--- events/ready.js
+++ events/ready.js
@@ -4,7 +4,6 @@ module.exports = async (client) => {
     console.log(`Logged in as ${client.user.tag}! (${client.user.id})`);
     pingDat.ping(process.env.PING_LOGIN_URL);
     client.user.setActivity(`${process.env.PREFIX}help | Serving the People`);
-
     setInterval((client) => {
         client.user.setActivity(`${process.env.PREFIX}help | Serving the People`);
     }, 21600*1000);
