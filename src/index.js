4b430bcbc7c4e0813b1fc7cdba99095b1d917844
Brandt <b.peters132@gmail.com>
2022-08-04 15:00:44 -0500

    Wap

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-console.log('things work!')
+import { Client, GatewayIntentBits } from 'discord.js'
+import dotenv from 'dotenv'
+dotenv.config()
+
+const token = process.env.TOKEN
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds
+    ]
+})
+
+client.once('ready', () => {
+    console.log('Ready!')
+})
+
+client.login(token)
