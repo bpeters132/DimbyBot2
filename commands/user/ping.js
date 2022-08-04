049ce4cf6ae031579cf941a3a1732fb8447618e9
Brandt <b.peters132@gmail.com>
2022-08-04 14:42:52 -0500

    REWRITE TIME

--- commands/user/ping.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: 'ping',
-    description: 'Ping!',
-    execute(client, message) {
-        message.reply(`Pong! Latency is ${Date.now() - message.createdTimestamp}ms. API Latency is ${Math.round(client.ws.ping)}ms`);
-    },
-};
-  
