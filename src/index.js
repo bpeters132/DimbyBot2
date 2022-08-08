e0a098181179dfc239af4dadbd29691908f95d28
Brandt <b.peters132@gmail.com>
2022-08-07 19:26:04 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import { Player } from 'discord-player';
 import musicPlayerEvents from './events/musicPlayerEvents.js';
 import loadEvents from './lib/loadEvents.js';
 import dotenv from 'dotenv';
-// import deployCommands from './lib/deployCommands.js';
-import deployCommandsDev from './lib/deployCommandsDev.js';
+import deployCommands from './lib/deployCommands.js';
+// import deployCommandsDev from './lib/deployCommandsDev.js';
 dotenv.config();
 
 const token = process.env.TOKEN;
@@ -18,8 +18,8 @@ async function main() {
         loadEvents(client);
         client.player = new Player(client);
         musicPlayerEvents(client.player);
-        // deployCommands();
-        deployCommandsDev();
+        deployCommands();
+        // deployCommandsDev();
         client.login(token);
     } catch (err) {
         console.error(err);
