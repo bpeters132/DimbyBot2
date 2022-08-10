5deb3c71dc27a533e14aadd83bc8c805e434cf5c
Brandt <b.peters132@gmail.com>
2022-08-10 10:10:30 -0500

    Update onError.js

--- src/events/onError.js
+++ src/events/onError.js
@@ -1,6 +1,6 @@
 export default async (client) => {
     console.log('Loading event on error');
-    client.on('error', () => {
-        console.error;
+    client.on('error', (err) => {
+        console.error(err);
     });
 };
