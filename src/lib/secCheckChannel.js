a35cdcf6eb880b1a16f122862ed3a7bac8c4c443
Brandt <b.peters132@gmail.com>
2022-10-20 14:54:40 -0500

    Update secCheckChannel.js

--- src/lib/secCheckChannel.js
+++ src/lib/secCheckChannel.js
@@ -1,5 +1,5 @@
 export default (client, message, guild) => {
-    return new Promise((resolve) =>{
+    return new Promise((resolve) => {
         // Check if the requesting member is in a voice channel
         if (!message.member.voice.channel) {
             message.reply('You have to be in a voice channel to do that!');
@@ -10,17 +10,19 @@ export default (client, message, guild) => {
         const queue = client.player.getQueue(guild);
         // console.log(queue);
         // if the queue exists or not
-        if (queue != undefined){
+        if (queue != undefined) {
             // console.log(`Queue Channel: ${queue.connection.channel}`);
             // console.log(`Member Channel: ${message.member.voice.channel}`);
             // if the requester is in the same voice channel as the bot or not
-            if (message.member.voice.channel == queue.connection.channel){
+            if (message.member.voice.channel == queue.connection.channel) {
                 return resolve(true);
-            }else{
+            }
+            else {
                 message.reply('You have to be in the same voice channel as the bot to do that!');
                 return resolve(false);
             }
-        }else{
+        }
+        else {
             return resolve(true);
         }
     });
