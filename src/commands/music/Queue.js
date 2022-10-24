c519d46e3e9bdb8158337e8fb4435bf1b3a94a88
Brandt <b.peters132@gmail.com>
2022-10-23 22:35:03 -0500

    continuing work on the queue command

--- src/commands/music/Queue.js
+++ src/commands/music/Queue.js
@@ -1,44 +1,86 @@
-import { SlashCommandBuilder } from 'discord.js';
+import { SlashCommandBuilder, ButtonBuilder } from 'discord.js';
 import secCheckChannel from '../../lib/secCheckChannel.js';
+import pagination from '@acegoal07/discordjs-pagination'
+
 
 class Queue extends SlashCommandBuilder {
     constructor() {
         super();
         super.setName('queue');
         super.setDescription('Get current queue');
-        super.addIntegerOption(option =>
-            option.setName('page').setDescription('Specific page number in queue').setRequired(false));
-
     }
     async run(client, message) {
-        let pageCount = message.options.getInteger('page');
-
         const queue = client.player.getQueue(message.guild.id);
         // if user asking command isn't in working channel, fail command
         const memberInChannel = await secCheckChannel(client, message, message.guild.id);
         if (!memberInChannel) return;
         if (!queue || !queue.playing) return void message.reply({ content: '❌ | No music is being played!' });
-        if (!pageCount) pageCount = 1;
-        const pageStart = 10 * (pageCount - 1);
-        const pageEnd = pageStart + 10;
         const currentTrack = queue.current;
-        const tracks = queue.tracks.slice(0, 10).map((m, i) => {
-            return `${i + 1}. **${m.title}** ([link](${m.url}))`;
-        });
 
-        return void message.reply({
-            embeds: [
+        const pages = []
+        const tracks = queue.tracks
+
+        for (let i = 0; i < tracks.length; i += 10) {
+            const fields = [
                 {
-                    title: 'Server Queue',
-                    description: `${tracks.join('\n')}${queue.tracks.length > pageEnd
-                        ? `\n...${queue.tracks.length - pageEnd} more track(s)`
-                        : ''
-                        }`,
-                    color: 0xff0000,
-                    fields: [{ name: 'Now Playing', value: `🎶 | **${currentTrack.title}** ([link](${currentTrack.url}))` }]
+                    name: 'Now Playing', value: `**${currentTrack.title}** ([link](${currentTrack.url}))`
                 }
             ]
-        });
+            const elements = queue.tracks.slice(i, i + 10).map((m, t) => {
+                return { title: `${t + 1} `, value: `**${m.title}** ([link](${m.url}))` }
+            });
+
+            elements.forEach(element => {
+                fields.push(element)
+            });
+
+            const page = {
+                color: 0xff0000,
+                title: 'Server Queue',
+                fields: fields
+            }
+
+            // console.log(`Page: ${page}`)
+
+            pages.push(page)
+        }
+
+        // console.log(`Pages: ${pages}`)
+        try {
+            console.log(JSON.parse(pages))
+            // pages[0].toJSON()
+        } catch (error) {
+            console.log(error)
+        }
+
+        new pagination().setInterface(message)
+            .createPages(JSON.stringify(pages))
+            .setButtonList([
+                new ButtonBuilder()
+                    .setLabel(`1`)
+                    .setStyle("Secondary")
+                    .setCustomId(`1`),
+                new ButtonBuilder()
+                    .setLabel(`2`)
+                    .setStyle("Secondary")
+                    .setCustomId(`2`)
+            ])
+            .paginate()
+
+
+        // return void message.reply('I did something! Look at the console!')
+
+
+
+        // return void message.reply({
+        //     embeds: [
+        //         {
+        //             title: 'Server Queue',
+        //             color: 0xff0000,
+        //             fields: [{ name: 'Now Playing', value: `🎶 | **${currentTrack.title}** ([link](${currentTrack.url}))` }]
+        //         }
+        //     ]
+        // });
 
 
     }
