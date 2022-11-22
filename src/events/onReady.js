e01c1c0c5313f5c0be52c52dd366e9916f2cd7c8
Brandt <b.peters132@gmail.com>
2022-11-21 21:26:19 -0600

    Updating packages
    Modified status to watching over amount of joined servers

--- src/events/onReady.js
+++ src/events/onReady.js
@@ -1,10 +1,14 @@
+import { ActivityType } from 'discord.js';
+
 export default async (client) => {
+    let serverCount = await client.guilds.cache.size;
     console.log('Loading event on ready');
     client.on('ready', () => {
         console.log(`Logged in as ${client.user.tag}! (${client.user.id})`);
-        client.user.setActivity('Serving the People');
-        setInterval(() => {
-            client.user.setActivity('Serving the People');
+        client.user.setActivity(`over ${serverCount} servers`, {type: ActivityType.Watching});
+        setInterval(async () => {
+            let serverCount = await client.guilds.cache.size;
+            client.user.setActivity(`over ${serverCount} servers`, {type: ActivityType.Watching});
         }, 21600 * 1000);
     });
 };
