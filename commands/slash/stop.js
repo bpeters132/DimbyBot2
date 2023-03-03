21b38da7bfac1dfa0f34774ade98d0cab9aa1ac0
Brandt <b.peters132@gmail.com>
2023-03-03 10:41:09 -0600

    nobody uses the buttons....

--- commands/slash/stop.js
+++ commands/slash/stop.js
@@ -6,50 +6,51 @@ const command = new SlashCommand()
 	.setDescription("Stops whatever the bot is playing and leaves the voice channel\n(This command will clear the queue)")
 	
 	.setRun(async (client, interaction, options) => {
-		let channel = await client.getChannel(client, interaction);
-		if (!channel) {
-			return;
-		}
+		interaction.reply('Please use the buttons for crying out loud!')
+		// let channel = await client.getChannel(client, interaction);
+		// if (!channel) {
+		// 	return;
+		// }
 		
-		let player;
-		if (client.manager) {
-			player = client.manager.players.get(interaction.guild.id);
-		} else {
-			return interaction.reply({
-				embeds: [
-					new MessageEmbed()
-						.setColor("RED")
-						.setDescription("Lavalink node is not connected"),
-				],
-			});
-		}
+		// let player;
+		// if (client.manager) {
+		// 	player = client.manager.players.get(interaction.guild.id);
+		// } else {
+		// 	return interaction.reply({
+		// 		embeds: [
+		// 			new MessageEmbed()
+		// 				.setColor("RED")
+		// 				.setDescription("Lavalink node is not connected"),
+		// 		],
+		// 	});
+		// }
 		
-		if (!player) {
-			return interaction.reply({
-				embeds: [
-					new MessageEmbed()
-						.setColor("RED")
-						.setDescription("I'm not in a channel."),
-				],
-				ephemeral: true,
-			});
-		}
+		// if (!player) {
+		// 	return interaction.reply({
+		// 		embeds: [
+		// 			new MessageEmbed()
+		// 				.setColor("RED")
+		// 				.setDescription("I'm not in a channel."),
+		// 		],
+		// 		ephemeral: true,
+		// 	});
+		// }
 		
-		if (player.twentyFourSeven) {
-			player.queue.clear();
-			player.stop();
-			player.set("autoQueue", false);
-		} else {
-			player.destroy();
-		}
+		// if (player.twentyFourSeven) {
+		// 	player.queue.clear();
+		// 	player.stop();
+		// 	player.set("autoQueue", false);
+		// } else {
+		// 	player.destroy();
+		// }
 		
-		interaction.reply({
-			embeds: [
-				new MessageEmbed()
-					.setColor(client.config.embedColor)
-					.setDescription(`:wave: | **Bye Bye!**`),
-			],
-		});
+		// interaction.reply({
+		// 	embeds: [
+		// 		new MessageEmbed()
+		// 			.setColor(client.config.embedColor)
+		// 			.setDescription(`:wave: | **Bye Bye!**`),
+		// 	],
+		// });
 	});
 
 module.exports = command;
